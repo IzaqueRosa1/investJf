2bc9ea707dcb464928ba36317548a3d52500c5f0
IzaqueRosa <r.izaqueso@gmail.com>
2023-07-08 12:30:37 -0300

    Adicionei menu hamburguer

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickMenu(){
+    itens.style.display = 'block'
+   if(iten.style.display === 'block') {
+    itens.style.display = 'none'
+   } else {
+    itens.style.display = 'block'
+   }
+}
